a06fcde39423c357a47b65335c0c8b07bfc5f874
nguyenmx <125946317+nguyenmx@users.noreply.github.com>
2023-05-16 20:52:52 -0700

    added spicy py

---
+++ pg_init/seed_data/kdrama.py
@@ -0,0 +1,13 @@
+from psycopg2.extensions import cursor
+
+from utils import parse_int
+
+def populate_kdrama(cur: cursor, rows: list[dict[str, str]]):
+    
+    for row in rows:
+        kdrama = (row['name']), parse_int(row['year']), parse_int['episode'], row['network'], row['content_rating'], row['cast'], parse_int['genre_rating'])
+        cur.execute("""
+            insert into kdrama(name, year, episode, network, content_rating, cast, genre_rating) 
+            values(%s, %s, %s, %s, %s, %s, %s)
+            on conflict do nothing
+            """, kdrama)
