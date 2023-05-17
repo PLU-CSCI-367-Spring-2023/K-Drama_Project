a51d226fbdc0b96ca09961e9cd1e70279a9c4a18
orangepulpsucks <42681894+orangepulpsucks@users.noreply.github.com>
2023-05-16 20:20:46 -0700

    TABLES

---
+++ schema/kDramaList.sql
@@ -0,0 +1,11 @@
+create table if not exists kDramaList (
+    kDramaName varchar not null,
+    yearOfRel int not null,
+    cast varchar not null,
+    genre varchar not null,
+    rating int not null,
+    numOfEp int not null,
+    duration int not null,
+    contentRate varchar not null,
+    rating int not null
+)
