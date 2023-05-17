81e41231fb4a1979fc0b540b69490d6c082da144
nguyenmx <125946317+nguyenmx@users.noreply.github.com>
2023-05-16 21:53:39 -0700

    updated kdrama.py

--- pg_init/seed_data/kdrama.py
+++ pg_init/seed_data/kdrama.py
@@ -1,11 +1,10 @@
 from psycopg2.extensions import cursor
-
 from utils import parse_int
 
 def populate_kdrama(cur: cursor, rows: list[dict[str, str]]):
     
     for row in rows:
-        kdrama = (row['name'], parse_int(row['year']), parse_int(['episode']), row['network'], row['content_rating'], row['cast'], parse_int(['genre_rating']))
+        kdrama = (row['name'], parse_int(row['year']), parse_int(row['episode']), row['network'], row['content_rating'], row['cast'], parse_int(row['genre_rating']))
         cur.execute("""
             insert into kdrama(name, year, episode, network, content_rating, cast, genre_rating) 
             values(%s, %s, %s, %s, %s, %s, %s)
