489f072e94623915fe5ba980f27d52d9580ed75e
nguyenmx <125946317+nguyenmx@users.noreply.github.com>
2023-05-18 12:13:17 -0700

    Updated the .sql and py to add all columns

--- .devcontainer/pg_init/01_create_tables.sql
+++ .devcontainer/pg_init/01_create_tables.sql
@@ -1,12 +1,16 @@
 create table kdrama (
     name text not null,
+    aired_date text not null,
+    week text not null,
     year int not null,
     episode int not null,
     network text not null,
     duration int not null,
-    content_rating int not null,
+    content int not null,
     synopsis text not null,
     casts text not null,
     genre text not null,
+    tags text not null,
+    rank text not null,
     rating float not null
 );
--- .devcontainer/pg_init/02_insert_rows.sql
+++ .devcontainer/pg_init/02_insert_rows.sql
@@ -1,4 +1,4 @@
-copy kdrama(name, year, episode, network, duration, content_rating, synopsis, casts, genre, rating)
+copy kdrama(name, aired_date, week, year, episode, network, duration, content, synopsis, casts, genre, tags, rank, rating)
 from '/docker-entrypoint-initdb.d/seed_data/top100_kdrama.csv'
 delimiter ','
 csv header;
