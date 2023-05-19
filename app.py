103493f3dbe2e0a7d82e952085600c608d635128
Anyah Arboine <aarboine@plu.edu>
2023-05-18 18:49:05 -0700

    can now optionally search by columns + I added more colors

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 import csv
-from flask import Flask, request
 from flask import Flask, request, render_template
 import psycopg2
+
 from psycopg2.extras import RealDictCursor
 
 conn = psycopg2.connect(
-    #just uncomment the proper host
-    #"host=db dbname=postgres user=postgres password=postgres", #//for docker users
-    "host=localhost dbname=final_project user=postgres password=postgres", #for Kirill
+  #just uncomment the proper host
+    "host=db dbname=postgres user=postgres password=postgres", #//for docker users
+    #"host=localhost dbname=final_project user=postgres password=postgres", #for Kirill
     cursor_factory=RealDictCursor)
 app = Flask(__name__)
-
-
 
 
 @app.route('/')
@@ -28,50 +26,72 @@ def render_sets():
     duration = request.args.get('duration')
     rating = request.args.get('content')
     network = request.args.get("network", "")
+    sort_by = request.args.get("sort_by", "name")
+    sort_dir = request.args.get("sort_dir", "asc")
+    page = request.args.get("page", 1, type=int)
+    limit = request.args.get("limit", 100, type=int)
+
+    #optional query params
+    from_where_clause = f"""
+            from kdrama 
+            where %(name)s is null or name ilike %(name)s
+            and ( %(year)s is null or year = %(year)s )
+            and ( %(episode)s is null or episode = %(episode)s )
+            and ( %(duration)s is null or duration = %(duration)s )
+            and ( %(rating)s is null or rating = %(rating)s )
+            and ( %(network)s is null or network ilike %(network)s )
+        """
 
     params = {
         "name": f"%{name}%",
         "year": int(year) if year else None,
         "casts": f"%{casts}%",
         "genre": f"%{genre}%",
-        "rating": int(rating) if rating else None,
+        "rating": float(rating) if rating else None,
         "episode": int(episode) if episode else None,
         "duration": int(duration) if duration else None,
-        "rating": int(rating) if rating else None,
-        "network": f"%{network}%"
+        "content": float(rating) if rating else None,
+        "network": f"%{network}%",
+        "sort_by": sort_by,
+        "sort_dir" : sort_dir,
+        "page" : page,
+        "limit" : limit
     }
 
-   
-    from_where_clause = """
-        from kdrama k
-        where k.name ilike %(name)s
-    """
 
-
-    with conn.cursor() as cur:
-        cur.execute(f"select k.name as name {from_where_clause}", params)
-        results = list(cur.fetchall())
+    # with conn.cursor() as cur:
+    #     #set_num
+    #     cur.execute(f"select s.set_num as set_num {from_where_clause}", params)
+    #     results_set_num = list(cur.fetchall())
         
     #     #listing all set_names and theme_names limit 100
     #     cur.execute(f"select s.name as set_name, t.name as theme_name, s.num_parts as part_count, s.set_num as set_num, s.year as year {from_where_clause}", params)
     #     sets = list(cur.fetchall())
-  
-       
-        #count counter
-        cur.execute(f"select count(*) as count {from_where_clause}", params)
-        count = cur.fetchone()["count"]
-    
+
+        
+    def get_sort_dir(col):
+        if col== sort_by:
+            return "desc" if sort_dir == "asc" else "asc"
+        else:
+            return "asc"
+
+ 
+    #count counter  
     with conn.cursor() as cur:
         cur.execute(f"""select name, year, casts, genre, episode, duration, rating, network
                         {from_where_clause} 
+                        order by {sort_by} {sort_dir} 
+                        limit %(limit)s 
                     """,
                     params)
         results = list(cur.fetchall())
 
-    return render_template("kdrama.html", 
+        cur.execute(f"select count(*) as count {from_where_clause}", params)
+        count = cur.fetchone()["count"]
+         
+    return render_template("kdrama.html",
                            kdrama=results,
                            params=request.args,
-                           result_count = count)
-
-
-
+                           get_sort_dir=get_sort_dir,
+                           result_count =count
+                           )
