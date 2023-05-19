114979d951ee4571c41f5ae770580d9da60ae92c
Kirill <kperfiliev@plu.edu>
2023-05-19 11:31:16 -0700

    added spicy button with decorations

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import csv
 from flask import Flask, request, render_template
-import psycopg2
+import psycopg2, random
 
 from psycopg2.extras import RealDictCursor
 
 conn = psycopg2.connect(
     #just uncomment the proper host
     #"host=db dbname=postgres user=postgres password=postgres", #//for docker users
-    #"host=localhost dbname=final_project user=postgres password=postgres", #for Kirill
+    "host=localhost dbname=final_project user=postgres password=postgres", #for Kirill
     cursor_factory=RealDictCursor)
 app = Flask(__name__)
 
@@ -15,6 +15,25 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return "Hello, World!"
+
+#list of all names in the dataset. I know there is easier way using sql, but I had problems with RealDictRow
+list_of_names = ["Move to Heaven", "Hospital Playlist", "Flower of Evil", "Hospital Playlist 2", "My Mister", "Prison Playbook",
+ "Reply 1988", "It's Okay to Not Be Okay", "Mr. Queen", "Mother", "Signal", "Navillera", "Happiness", "Vincenzo",
+ "Crash Landing on You", "SKY Castle", "Kingdom", "Mr. Sunshine", "Healer", "Stranger", "Goblin", "Hometown Cha-Cha-Cha", 
+ "The Uncanny Counter", "D.P.", "Kingdom", "The Penthouse", "Weightlifting Fairy Kim Bok Joo", "The Devil Judge",
+ "Life on Mars", "The Penthouse 2", "Beyond Evil", "Taxi Driver", "Six Flying Dragons", "The Guest", "Racket Boys", 
+ "Youth of May", "Mouse", "Sweet Home", "18 Again", "Chicago Typewriter", "Defendant", "While You Were Sleeping", 
+ "Dear My Friends", "Dr. Romantic 2", "Kill Me, Heal Me", "Arthdal Chronicles Part 2", "Misaeng", "Arthdal Chronicles Part 3", 
+ "My Name", "Hotel del Luna", "Dr. Romantic", "The Fiery Priest", "Moon Lovers", "Descendants of the Sun", 
+ "Hot Stove League", "Strong Woman Do Bong Soon", "Law School", "Live", "Tunnel", "Strangers from Hell", "It's Okay, That's Love", 
+ "Eulachacha Waikiki", "The Bridal Mask", "Partners for Justice 2", "Jewel in the Palace", "My Father is Strange", 
+ "Children of Nobody", "Go Back Couple", "Good Manager", "Stranger 2", "My Love from the Star", "Empress Ki", 
+ "Age of Youth", "Mystic Pop-Up Bar", "Cruel City", "365", "Designated Survivor", "Just Between Lovers", "Squid Game", 
+ "Avengers Social Club", "The Master's Sun", "Once Again", "Save Me", "Bad Guys", "Missing", "Rebel", "Nobody Knows", 
+ "I Hear Your Voice", "Reply 1997", "Kairos", "Dali and the Cocky Prince", "Yumi's Cells", "Arthdal Chronicles Part 1", 
+ "Beautiful World", "What's Wrong with Secretary Kim", "Vagabond", "Doctor John", "When the Camellia Blooms",
+ "Because This Is My First Life", "Fight For My Way"]
+
 
 @app.route("/home")
 def render_sets():
@@ -85,12 +104,23 @@ def render_sets():
         cur.execute(f"select count(*) as count {from_where_clause}", params)
         count = cur.fetchone()["count"]
          
+
+    
+
+    def need_some_spiciness():
+        index = random.randrange(0, 99)
+        return list_of_names[index]
+    
     return render_template("kdrama.html",
                            kdrama=results,
                            params=request.args,
                            get_sort_dir=get_sort_dir,
-                           result_count =count
+                           result_count =count,
+                           spicy = need_some_spiciness()
                            )
+
+
+
 @app.route("/synopsis")
 def render_sets2():
     name = "%"+request.args.get("name", "")+"%"
